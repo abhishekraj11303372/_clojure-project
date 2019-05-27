7e9b575eb6d6435158d3bf347b9aeacfcbf05135
abhishek raj <abhishek.raj@marlabs.com>
2019-05-27 17:05:51 +0530

    master

--- project.clj
+++ project.clj
@@ -4,10 +4,11 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :plugins [[lein-ring "0.12.5"]]
-  :ring {:handler clojure-project.core/my-routes
+  :ring {:handler clojure-project.core/app
          :auto-reload? true
          :auto-refresh? false}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring "1.3.2"]
+                 [ring/ring-json "0.2.0"]
                  [compojure "1.2.0-SNAPSHOT"]])
 
--- src/clojure_project/core.clj
+++ src/clojure_project/core.clj
@@ -1,9 +1,15 @@
 (ns clojure-project.core
-  ( :use compojure.core)
-  ( :require [compojure.route :as route]))
+  ( :use compojure.core
+         ring.util.response
+         ring.middleware.json)
+  ( :require [compojure.route :as route]
+             [clojure-project.view :as view] ))
 
 (defn foo [x] (str x "Hello," x))
 
 (defroutes my-routes
-           (GET "/" [] (foo "Jim"))
+           (GET "/" [] (view/index-page))
+           (GET "/rest" [] (response {:email "abhishek.raj@marlabs.com"}))
            (route/resources "/"))
+
+(def app (wrap-json-response my-routes))
